d53e87dacbacc1371e49a8e35e317dc5e269e991
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-05-04 22:14:11 +0530

    Made external as the landing page previously My Library was the landing page and made some change in the myLibrary.json

--- src/App.js
+++ src/App.js
@@ -17,43 +17,49 @@ function App() {
   const [navbar, setNavbar] = useState({ backgroundImage: "linear-gradient(to top right, white  , rgba(132, 139, 200, 0.18))", position: "sticky", top: "0px", zIndex: "2" })
   const [toggling, setToggling] = useState("light");
 
-  const [lib, setLib] = useState(true);
-  const [content, displayContent] = useState(false);
-  const [togglingButton, setTogglingButton] = useState("External");
+  const [lib, setLib] = useState(false);
+  const [content, displayContent] = useState(true);
+  const [togglingButton, setTogglingButton] = useState("My Library");
   const [pathContent, setPathContent] = useState(false);
   const [appInsightContent, setappInsightContent] = useState(false);
   const [sqlContent, setSqlContent] = useState(false);
-  const [pathButton, setPathButton] = useState(true);
-  const [appInsightButton, setappInsightButton] = useState(true);
-  const [sqlButton, setSqlButton] = useState(true);
+  const [sumoContent, setSumoContent] = useState(false);
+  const [pathButton, setPathButton] = useState(false);
+  const [appInsightButton, setappInsightButton] = useState(false);
+  const [sqlButton, setSqlButton] = useState(false);
+  const [sumoButton, setSumoButton] = useState(false);
+
 
   function Display() {
 
-    if (lib === true) {
-      setLib(false);
-      displayContent(true);
-      setTogglingButton("My Library");
-      setPathButton(false);
-      setappInsightButton(false);
-      setSqlButton(false);
-      setPathContent(false);
-      setappInsightContent(false)
-      setSqlContent(false)
-   
-
-    }
-    else {
-
+    if (content === true) {
       setLib(true);
       displayContent(false);
       setTogglingButton("External");
       setPathButton(true);
       setappInsightButton(true);
       setSqlButton(true);
+      setSumoButton(true);
       setPathContent(false);
       setappInsightContent(false)
       setSqlContent(false)
-  
+      setSumoContent(false);
+
+    }
+    else {
+
+      setLib(false);
+      displayContent(true);
+      setTogglingButton("My Library");
+      setPathButton(false);
+      setappInsightButton(false);
+      setSqlButton(false);
+      setSumoButton(false);
+      setPathContent(false);
+      setappInsightContent(false)
+      setSqlContent(false)
+      setSumoContent(false);
+
     }
   }
 
@@ -63,7 +69,8 @@ function App() {
     setappInsightContent(false)
     setSqlContent(false)
     setLib(false)
-    
+    setSumoContent(false);
+
   }
 
   function DisplayAppInsight() {
@@ -71,6 +78,7 @@ function App() {
     setappInsightContent(true)
     setSqlContent(false)
     setLib(false)
+    setSumoContent(false);
 
   }
 
@@ -79,9 +87,17 @@ function App() {
     setappInsightContent(false)
     setSqlContent(true)
     setLib(false)
+    setSumoContent(false);
 
   }
 
+  function DisplaySumo() {
+    setPathContent(false);
+    setappInsightContent(false)
+    setSqlContent(false)
+    setLib(false)
+    setSumoContent(true);
+  }
 
   function toggle(data) {
     if (data === "light") {
@@ -96,7 +112,7 @@ function App() {
     }
     else {
 
-      setLightToggle({ backgroundColor: "transparent", color: "white",cursor: "pointer" })
+      setLightToggle({ backgroundColor: "transparent", color: "white", cursor: "pointer" })
       setDarkToggle({ backgroundColor: "#7388ec", cursor: "pointer" })
       document.body.style.backgroundColor = "#181a1e"
       setToggleStyle({ backgroundColor: "#424242" })
@@ -122,10 +138,13 @@ function App() {
 
       {sqlContent && <SqlData cardColor={cardColor} />}
 
-      <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "55px", left: "5px", zIndex: "2",padding:"7px" }} onClick={Display}>{togglingButton}</button>
-      {pathButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "107px", left: "5px", zIndex: "2", padding:"7px" }} onClick={DisplayPath}>Paths</button>}
-      {appInsightButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "5px", left: "5px", zIndex: "2", padding:"7px" }} onClick={DisplayAppInsight}>AppInsight</button>}
-      {sqlButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "165px", left: "5px", zIndex: "2", padding:"7px" }} onClick={DisplaySql}>Sql</button>}
+      {sumoContent && <Library cardColor={cardColor} />}
+
+      <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "107px", left: "5px", zIndex: "2", padding: "7px" }} onClick={Display}>{togglingButton}</button>
+      {pathButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "160px", left: "5px", zIndex: "2", padding: "7px" }} onClick={DisplayPath}>Paths</button>}
+      {appInsightButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "55px", left: "5px", zIndex: "2", padding: "7px" }} onClick={DisplayAppInsight}>AppInsight</button>}
+      {sqlButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "210px", left: "5px", zIndex: "2", padding: "7px" }} onClick={DisplaySql}>Sql</button>}
+      {sumoButton && <button className="btn btn-sm btn-warning rounded-circle" style={{ position: "fixed", bottom: "5px", left: "5px", zIndex: "2", padding: "7px" }} onClick={DisplaySumo}>Sumo Query</button>}
     </>
 
   );
