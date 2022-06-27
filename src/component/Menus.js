45cdc2676613c721ca3546ef85e2d0662ac9e4da
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-06-27 17:02:57 +0530

    Changed the height and made some other UI level changes

--- src/component/Menus.js
+++ src/component/Menus.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function Menus(props) {
@@ -9,7 +9,7 @@ export default function Menus(props) {
 
     const changes = (route) => {
 
-        props.setStatus(false)
+        //props.setStatus(false)
         props.show();
         navigate(route);
 
@@ -18,54 +18,62 @@ export default function Menus(props) {
 
     return (
         <div className={props.classname}>
-            <div className="menuitems p-3" onClick={() => changes('/triage')} >
+            <div className="menuitems" onClick={() => changes('/triage')} >
                 <span className="material-symbols-outlined">
                     plagiarism
                 </span>
-                <Link to="/triage">Triage</Link>
+                {/* <Link to="/triage">Triage</Link> */}
+                Triage
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/')}>
+            <div className="menuitems" onClick={() => changes('/')}>
                 <span className="material-symbols-outlined">
                     elderly_woman
                 </span>
-                <Link to="/">Ad. Sumo</Link>
+                {/* <Link to="/">Ad. Sumo</Link> */}
+                Ad. Sumo
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/opscentral')}>
+            <div className="menuitems" onClick={() => changes('/opscentral')}>
                 <span className="material-symbols-outlined">
                     pending_actions
                 </span>
-                <Link to="/opscentral">Ops Central</Link>
+                {/* <Link to="/opscentral">Ops Central</Link> */}
+                Ops Central
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/criticalsystem')}>
+            <div className="menuitems" onClick={() => changes('/criticalsystem')}>
                 <span className="material-symbols-outlined">
                     detector_alarm
                 </span>
-                <Link to="/criticalsystem">Critical System</Link>
+                {/* <Link to="/criticalsystem">Critical System</Link> */}
+                Critical System
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/sql')}>
+            <div className="menuitems" onClick={() => changes('/sql')}>
                 <span className="material-symbols-outlined">
                     key_visualizer
                 </span>
-                <Link to="/sql">My Query</Link>
+                {/* <Link to="/sql">My Query</Link> */}
+                My Query
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/paths')}>
+            <div className="menuitems" onClick={() => changes('/paths')}>
                 <span className="material-symbols-outlined">
                     travel_explore
                 </span>
-                <Link to="/paths">My Paths</Link>
+                {/* <Link to="/paths">My Paths</Link> */}
+                My paths
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/appinsight')}>
+            <div className="menuitems" onClick={() => changes('/appinsight')}>
                 <span className="material-symbols-outlined">
                     data_thresholding
                 </span>
 
-                <Link to="/appinsight">My AppInsight</Link>
+                {/* <Link to="/appinsight">My AppInsight</Link> */}
+                My AppInsight
             </div>
-            <div className="menuitems p-3" onClick={() => changes('/sumo')}>
+            <div className="menuitems" onClick={() => changes('/sumo')}>
                 <span className="material-symbols-outlined">
                     grading
                 </span>
-                <Link to="/sumo">My Sumo</Link>
+                {/* <Link to="/sumo">My Sumo</Link> */}
+                My Sumo
             </div>
         </div>
     )
