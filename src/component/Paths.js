0799cb9d7b9056fe0363e3e909beeb3a176e3abd
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-12 22:20:35 +0530

    Integrated Path API

--- src/component/Paths.js
+++ src/component/Paths.js
@@ -1,28 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import PathItem from './PathItem.js'
+import Spinner from './Spinner.js';
 
 export default function PathContent(props) {
 
   const [query, setQuery] = useState([]);
+  const [apiStatus, setApiStatus] = useState(200);
+  const [loading, setLoading] = useState(true);
 
   async function Fetchdata() {
-    let data = await fetch('./Paths.json'
-      , {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-    );
 
-    let dataJson = await data.json();
+    /*****************************File System*****************************/
+    // let data = await fetch('./Paths.json'
+    //   , {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json'
+    //     }
+    //   }
+    // );
 
-    setQuery(dataJson.paths);
+
+    /*****************************DB System*****************************/
+
+    let data = await fetch('https://flightops.vercel.app/api/v1/fops/contents/paths')
+
+    if (data.status === 200) {
+      let dataJson = await data.json();
+
+      setQuery(dataJson.data.path);
+
+    }
+    else {
+
+
+      // If the status is anything other than 200 like 500, 404 504 then status state will be updated and accordingly the 
+      // alert will also be shown and the logic is written in the down JSX part.
+      setApiStatus(data.status);
+    }
+
+
+    // setLoading() can't be kept after FetchData() in use Effcet. As the fetchData() is async after calling out the api the control
+    // comes out of the function and thus making setLoading(false) and hence we will not be able to see the spinner.
+    setLoading(false);
+
+
+    // If the status is other than 200 then automatically the query array will be an empty array
+
   }
 
   useEffect(() => {
     Fetchdata();
-  },[])
+
+    // setLoading(false);
+  }, [])
 
 
   async function copyQuery(id1, id2) {
@@ -43,11 +74,27 @@ export default function PathContent(props) {
       <div className="container">
         <div className="row">
 
-          {query.map((data) =>
-            <div key={data.name} className="col-12 my-5">
-              <PathItem data={data} copyQuery={copyQuery} cardColor={props.cardColor} />
+
+          {loading && <Spinner />}
+
+
+          {/* First check if the array is empty or not and as well check if the status is 200 or not. If the array is emplty and the 
+          status is also different other than 200 then only alert will be shown*/}
+
+
+          {query.length === 0 && apiStatus !== 200 ?
+
+            <div class="alert alert-danger text-center" role="alert">
+              <strong>Something went wrong in the backend !! Please reload the webpage.</strong>
             </div>
-          )}
+
+            :
+
+            query.map((data) =>
+              <div key={data.name} className="col-12 my-5">
+                <PathItem data={data} copyQuery={copyQuery} cardColor={props.cardColor} />
+              </div>
+            )}
 
         </div>
       </div>
