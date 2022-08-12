9748974c319cc35eebde76baed607c827f62c09b
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-12 23:03:17 +0530

    Created Informix DB Query API

--- src/component/SqlData.js
+++ src/component/SqlData.js
@@ -1,27 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import PathItem from './PathItem.js'
+import Spinner from './Spinner.js';
+
+
 export default function SqlContent(props) {
 
   const [query, setQuery] = useState([]);
+  const [apiStatus, setApiStatus] = useState(200);
+  const [loading, setLoading] = useState(true);
 
   async function FetchData() {
-    let data = await fetch('./DBQuery.json'
-      , {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-    );
 
-    let dataJson = await data.json();
+    /*****************************File System*****************************/
+    // let data = await fetch('./DBQuery.json'
+    //   , {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json'
+    //     }
+    //   }
+    // );
 
-    setQuery(dataJson.paths);
+
+
+    /*****************************DB System*****************************/
+
+
+    const data = await fetch('https://flightops.vercel.app/api/v1/fops/contents/dbQuer')
+
+
+    if (data.status === 200) {
+      let dataJson = await data.json();
+
+      setQuery(dataJson.data.dbQuery);
+    }
+
+    else {
+      setApiStatus(data.status)
+    }
+
+
+    // setLoading() can't be kept after FetchData() in use Effcet. As the fetchData() is async after calling out the api the control
+    // comes out of the function and thus making setLoading(false) and hence we will not be able to see the spinner.
+    setLoading(false)
+
+
+
+    // If the status is other than 200 then automatically the query array will be an empty array
+
   }
 
 
   useEffect(() => {
     FetchData();
+
+    // setLoading(false);
   }, [])
 
   async function copyQuery(id1, id2) {
@@ -41,15 +74,31 @@ export default function SqlContent(props) {
   return (
     <>
 
-      <div className="container">
+      <div className="container my-3">
         <div className="row">
 
-          {query.map((data) =>
 
-            <div key={data.name} className="col-12 my-5">
-              <PathItem key={data.name} data={data} copyQuery={copyQuery} cardColor={props.cardColor} />
+          {loading && <Spinner />}
+
+
+          {/* First check if the array is empty or not and as well check if the status is 200 or not. If the array is emplty and the 
+          status is also different other than 200 then only alert will be shown*/}
+
+
+          {query.length === 0 && apiStatus !== 200 ?
+
+            <div class="alert alert-danger text-center" role="alert">
+              <strong>Something went wrong in the backend !! Please reload the webpage.</strong>
             </div>
-          )}
+
+            :
+
+            query.map((data) =>
+
+              <div key={data.name} className="col-12 my-5">
+                <PathItem key={data.name} data={data} copyQuery={copyQuery} cardColor={props.cardColor} />
+              </div>
+            )}
         </div>
       </div>
     </>
