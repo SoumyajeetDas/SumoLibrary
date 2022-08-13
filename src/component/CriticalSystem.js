e7041718a3187c35f48c6418d454882bbc4fee85
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-13 12:02:37 +0530

    Added CriticalSystem API

--- src/component/CriticalSystem.js
+++ src/component/CriticalSystem.js
@@ -1,42 +1,85 @@
 import React, { useEffect, useState } from 'react'
 import CentralOpsLink from './CriticalSystemLinks.js'
-
+import Spinner from './Spinner.js';
 
 export default function CentralOpsContent(props) {
 
-  const [query, setQuery] = useState([])
+  const [query, setQuery] = useState([]);
+  const [apiStatus, setApiStatus] = useState(200);
+  const [loading, setLoading] = useState(true);
+
 
   async function FetchData() {
-    let data = await fetch('./criticalLinks.json'
-      , {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-    );
 
-    let dataJson = await data.json();
+    /*****************************File System*****************************/
+    // let data = await fetch('./criticalLinks.json'
+    //   , {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Accept': 'application/json'
+    //     }
+    //   }
+    // );
 
-    setQuery(dataJson.links);
+
+    /*****************************DB System*****************************/
+
+    const data = await fetch('https://flightops.vercel.app/api/v1/fops/contents/criticalsystem');
+
+
+    if (data.status === 200) {
+      let dataJson = await data.json();
+
+      setQuery(dataJson.data.criticalSystem);
+    }
+    else {
+      // If the status is anything other than 200 like 500, 404 504 then status state will be updated and accordingly the 
+      // alert will also be shown and the logic is written in the down JSX part.
+      setApiStatus(data.status);
+    }
+
+
+    // setLoading() can't be kept after FetchData() in use Effcet. As the fetchData() is async after calling out the api the control
+    // comes out of the function and thus making setLoading(false) and hence we will not be able to see the spinner.
+    setLoading(false);
+
+
+    // If the status is other than 200 then automatically the query array will be an empty array
   }
 
   useEffect(() => {
     FetchData();
-  },[])
+
+    // setLoading(false);
+  }, [])
 
 
   return (
     <>
 
-      <div className="container">
+      <div className="container my-3">
         <div className="row">
 
-          {query.map((data) =>
-            <div key={data.name} className="col-md-4 my-5">
-              <CentralOpsLink data={data} cardColor={props.cardColor} />
+
+          {loading && <Spinner />}
+
+
+          {/* First check if the array is empty or not and as well check if the status is 200 or not. If the array is emplty and the 
+          status is also different other than 200 then only alert will be shown*/}
+
+          {query.length === 0 && apiStatus !== 200 ?
+
+            <div class="alert alert-danger text-center" role="alert">
+              <strong>Something went wrong in the backend !! Please reload the webpage.</strong>
             </div>
-          )}
+
+            :
+
+            query.map((data) =>
+              <div key={data.name} className="col-md-4 my-5">
+                <CentralOpsLink data={data} cardColor={props.cardColor} />
+              </div>
+            )}
 
         </div>
       </div>
