91c80aadb5cf2c594899611038f6db7c14139e11
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-10-03 13:14:06 +0530

    Change in the logic of AddSev and UpdateSev

--- src/component/maindata/UpdateSev.js
+++ src/component/maindata/UpdateSev.js
@@ -2,23 +2,43 @@ import React, { useState, useEffect } from 'react'
 import SaveSevSpinner from './SaveSevSpinner';
 import { useLocation } from "react-router-dom";
 
-export default function AddSev(props) {
 
-    const [title, setTitle] = useState("");
-    const [ticketNo, setTicketNo] = useState("");
-    const [miNo, setMINo] = useState("");
-    const [appType, setAppType] = useState("");
-    const [tickType, setTickType] = useState("");
-    const [timeOccured, setTimeOccured] = useState("");
-    const [description, setDescription] = useState("");
-    const [link, setLink] = useState("");
+const isEmpty = value => value.trim() === '';
 
-    const [titleError, setTitleError] = useState(undefined);
-    const [ticketError, setTicketError] = useState(undefined);
-    const [miError, setMiError] = useState(undefined);
-    const [appTypeError, setAppTypeError] = useState(undefined);
-    const [ticketTypeError, setTicketTypeError] = useState(undefined);
-    const [dateOccurError, setDateOccurError] = useState(undefined);
+export default function UpdateSev(props) {
+
+    const [formData, setFormData] = useState({
+        title: '',
+        ticketNo: '',
+        miNo: '',
+        appType: '',
+        tickType: '',
+        timeOccured: '',
+        description: '',
+        link: ''
+    });
+
+    const [formValididty, setFormValididty] = useState({
+        title: true,
+        ticketNo: true,
+        miNo: true,
+        appType: true,
+        tickType: true,
+        timeOccured: true,
+        description: true,
+        link: true
+    });
+
+
+    const handleForm = (e) => {
+        setFormData((prevData) => {
+            return {
+                ...prevData,
+                [e.target.id]: e.target.value
+            }
+
+        })
+    }
 
     const [save, setSave] = useState(false);
 
@@ -32,88 +52,6 @@ export default function AddSev(props) {
     // This hook will get all the data passed from useNavigate used in Sev.js
     const location = useLocation();
 
-    const handleTitle = () => {
-        if (title.length === 0) {
-            setTitleError("Please provide a Title")
-        }
-        else {
-            setTitleError('')
-        }
-    }
-
-    const handleTicketNumber = () => {
-        if (ticketNo.length === 0) {
-            setTicketError("Please provide a Ticket Number or write N/A")
-        }
-        else {
-            setTicketError("")
-        }
-    }
-
-    const handleMi = () => {
-        if (miNo.length === 0) {
-            setMiError("Please provide a MI or write N/A")
-        }
-        else {
-            setMiError("")
-        }
-    }
-
-    const handleAppType = () => {
-
-        // For the first time when nothing is givem for select tag the appType will be "" while if we select a option and 
-        // again change it back to Select the type of application the value of appType returned will be none() as given in the option.
-        // <option selected value="none">Select the type of Application</option>
-
-        if (appType.length === 0 || appType === "none") {
-            setAppTypeError("Please provide a App type")
-        }
-        else {
-            setAppTypeError("")
-        }
-    }
-
-    const handleTicketType = () => {
-        // For the first time when nothing is givem for select tag the tickType will be "" while if we select a option and 
-        // again change it back to Select the type of application the value of tickType returned will be none() as given in the option.
-        // <option selected value="none">Select the type of Ticket</option>
-        if (tickType.length === 0 || tickType === "none") {
-            setTicketTypeError("Please provide a Ticket Type")
-        }
-        else {
-            setTicketTypeError("")
-        }
-    }
-
-    const handleTimeOccured = () => {
-        if (timeOccured.length === 0) {
-            setDateOccurError("Please provide the date sev occured")
-        }
-        else {
-            setDateOccurError("")
-        }
-    }
-
-
-
-
-    /******************** The checking function is called by the update button onClick Event ******************************/
-
-    const checking = () => {
-
-        window.scrollTo(0, 0); // Scroll to the top
-
-        // I kept all these functions inside submit but if I do that I have to press 2 times submit button. So I need to make a 
-        // separate function to execute the function when submit button is clicked and if all ok the flag will be 0 and submit 
-        // will work.
-        handleTitle();
-        handleTicketNumber();
-        handleMi();
-        handleAppType();
-        handleTicketType();
-        handleTimeOccured();
-    }
-
 
 
     const updateTicket = async () => {
@@ -122,14 +60,14 @@ export default function AddSev(props) {
         setError(false);
 
         let data = {
-            title,
-            tickNos: ticketNo,
-            miNos: miNo,
-            applications: appType,
-            ticketType: tickType,
-            timeOccured,
-            description,
-            link
+            title: formData.title,
+            tickNos: formData.ticketNo,
+            miNos: formData.miNo,
+            applications: formData.appType,
+            ticketType: formData.tickType,
+            timeOccured: formData.timeOccured,
+            description: formData.description,
+            link: formData.link
         }
 
         let res = await fetch(`https://flightops.vercel.app/api/v1/fops/sevs/${location.state._id}`, {
@@ -165,48 +103,57 @@ export default function AddSev(props) {
 
     const submit = (e) => {
 
-        if (titleError === "" && ticketError === "" && miError === "" && appTypeError === "" && ticketTypeError === "" && dateOccurError === "") {
+        e.preventDefault();
 
-            updateTicket();
+        window.scrollTo(0, 0); // Scroll to the top
+
+        const isTitleValid = !isEmpty(formData.title);
+        const isTicketNoValid = !isEmpty(formData.ticketNo);
+        const isMiNoValid = !isEmpty(formData.miNo);
+        const isAppTypeValid = !isEmpty(formData.appType);
+        const isTickTypeValid = !isEmpty(formData.tickType);
+        const isTimeOccuredValid = !isEmpty(formData.timeOccured);
+
+        setFormValididty({
+            title: isTitleValid,
+            ticketNo: isTicketNoValid,
+            miNo: isMiNoValid,
+            appType: isAppTypeValid,
+            tickType: isTickTypeValid,
+            timeOccured: isTimeOccuredValid,
+        })
+
+        const formValid = isTitleValid && isTicketNoValid && isMiNoValid && isAppTypeValid && isTickTypeValid && isTimeOccuredValid
+
+
+        if (!formValid) {
+            console.log("Not Valid")
+            return;
         }
 
-        e.preventDefault();
+        updateTicket();
     }
+
 
 
     // This function will load the data into text fields that is coming from the SevComponents with the help of useNavigate() and
     // useLocation hook.
-    const LoadData = () => {
-
-        setTitle(location.state.title);
-
-
-        setTicketNo(location.state.tickNos)
-
-
-        setMINo(location.state.miNos)
-
-        setAppType(location.state.applications)
-
-        if (location.state.ticketType)
-            setTickType(location.state.ticketType)
-
-
+    useEffect(() => {
 
         // location.state.timeOccured = 2021-06-19T00:00:00.000Z
         let date = location.state.timeOccured.split("T"); // ['2021-06-19', '00:00:00.000Z']
-        setTimeOccured(date[0]);
 
 
-        setDescription(location.state.description);
-
-
-        setLink(location.state.link);
-    }
-
-    useEffect(() => {
-
-        LoadData();
+        setFormData({
+            title: location.state.title,
+            ticketNo: location.state.tickNos,
+            miNo: location.state.miNos,
+            appType: location.state.applications,
+            tickType: location.state.ticketType,
+            timeOccured: date[0],
+            description: location.state.description,
+            link: location.state.link
+        })
 
 
     }, // eslint-disable-next-line
@@ -241,23 +188,23 @@ export default function AddSev(props) {
                                 <h2 className="text-center formHeading m-4">Severity Tickets Form</h2>
                                 <div className="m-4">
                                     <label htmlFor="title" className="form-label">Title</label>
-                                    <input value={title} type="text" className="form-control" id="title" placeholder="Enter the title of the Ticket" onChange={(e) => setTitle(e.target.value)} />
-                                    <b className="text-danger">{titleError}</b>
+                                    <input value={formData.title} type="text" className="form-control" id="title" placeholder="Enter the title of the Ticket" onChange={handleForm} />
+                                    {!formValididty.title && <b className="text-danger">Please enter the title</b>}
                                 </div>
                                 <div className="m-4">
                                     <label htmlFor="ticketNo" className="form-label">Ticket Number</label>
-                                    <input value={ticketNo} type="text" className="form-control" id="ticketNo" placeholder="Enter the Ticket Number if not present provide N/A" onChange={(e) => setTicketNo(e.target.value)} />
-                                    <b className="text-danger">{ticketError}</b>
+                                    <input value={formData.ticketNo} type="text" className="form-control" id="ticketNo" placeholder="Enter the Ticket Number if not present provide N/A" onChange={handleForm} />
+                                    {!formValididty.ticketNo && <b className="text-danger">Please enter the Ticket No.</b>}
                                 </div>
                                 <div className="m-4">
                                     <label htmlFor="miNo" className="form-label">Major Incident Number</label>
-                                    <input value={miNo} type="text" className="form-control" id="miNo" placeholder="Enter the MI Number if not present provide N/A" onChange={(e) => setMINo(e.target.value)} />
-                                    <b className="text-danger">{miError}</b>
+                                    <input value={formData.miNo} type="text" className="form-control" id="miNo" placeholder="Enter the MI Number if not present provide N/A" onChange={handleForm} />
+                                    {!formValididty.miNo && <b className="text-danger">Please enter the Mi No.</b>}
                                 </div>
                                 <div className="m-4">
                                     <label className="form-label">Application Type</label>
-                                    <select value={appType} className="form-select" id="appType" onChange={(e) => setAppType(e.target.value)}>
-                                        <option value="none">Select the type of Application</option>
+                                    <select value={formData.appType} className="form-select" id="appType" onChange={handleForm}>
+                                        <option value="">Select the type of Application</option>
                                         <option value="ACARS">ACARS</option>
                                         <option value="Aircraft Performance">Aircraft Performance</option>
                                         <option value="FOGS">FOGS</option>
@@ -273,32 +220,32 @@ export default function AddSev(props) {
                                         <option value="Related to other Team">Related to other Team</option>
                                         <option value="Related to other Team">Multiple System Down</option>
                                     </select>
-                                    <b className="text-danger">{appTypeError}</b>
+                                    {!formValididty.appType && <b className="text-danger">Please enter the App Type</b>}
                                 </div>
                                 <div className="m-4">
                                     <label className="form-label">Ticket Type</label>
-                                    <select value={tickType} className="form-select" id="tickType" onChange={(e) => setTickType(e.target.value)}>
-                                        <option value="none">Select the type of Ticket</option>
+                                    <select value={formData.tickType} className="form-select" id="tickType" onChange={handleForm}>
+                                        <option value="">Select the type of Ticket</option>
                                         <option value="Sev1">Sev1</option>
                                         <option value="Sev2">Sev2</option>
                                     </select>
-                                    <b className="text-danger">{ticketTypeError}</b>
+                                    {!formValididty.tickType && <b className="text-danger">Please enter the Ticket Type</b>}
                                 </div>
                                 <div className="m-4">
                                     <label htmlFor="timeOccured" className="form-label">Occuernce Date</label>
-                                    <input value={timeOccured} id="timeOccured" type="date" className="form-control" onChange={(e) => setTimeOccured(e.target.value)} />
-                                    <b className="text-danger">{dateOccurError}</b>
+                                    <input value={formData.timeOccured} id="timeOccured" type="date" className="form-control" onChange={handleForm} />
+                                    {!formValididty.timeOccured && <b className="text-danger">Please enter the Data Occured</b>}
                                 </div>
                                 <div className="m-4">
                                     <label htmlFor="description" className="form-label">Description</label>
-                                    <textarea value={description} className="form-control" id="description" rows="4" placeholder="Enter the description of ticket or write N/A" onChange={(e) => setDescription(e.target.value)}></textarea>
+                                    <textarea value={formData.description} className="form-control" id="description" rows="4" placeholder="Enter the description of ticket or write N/A" onChange={handleForm}></textarea>
                                 </div>
                                 <div className="m-4">
                                     <label htmlFor="link" className="form-label">Url Link</label>
-                                    <input value={link} type="text" className="form-control" id="link" placeholder="Enter the url of the document" onChange={(e) => setLink(e.target.value)} />
+                                    <input value={formData.link} type="text" className="form-control" id="link" placeholder="Enter the url of the document" onChange={handleForm} />
                                 </div>
                                 <div className="m-4">
-                                    <button className="btn btn-success" onClick={checking}>Update</button>
+                                    <button className="btn btn-success" type='submit'>Update</button>
                                 </div>
                             </div>
                         </form>
