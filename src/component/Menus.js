6ad16489c90ba92d432199c4e9d1714da05d043b
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-11-21 21:26:20 +0530

    Made Menus active when the component is mounted

--- src/component/Menus.js
+++ src/component/Menus.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useResolvedPath, useMatch } from 'react-router-dom'
 import { motion } from 'framer-motion';
 
 
@@ -32,6 +32,32 @@ export default function Menus(props) {
     const navigate = useNavigate();
 
 
+    // When any nav link will be selected that particular component will be rendered and rendered link will be added the className active 
+    // so that the colour gets changed from white to yellow
+    const CustomLink = (props) => {
+        // The resolvedPath resolves a given To value into an actual Path object with an absolute pathname. This is useful whenever 
+        // you need to know the exact path for a relative To value. For example, the <Link> component uses this function to know the
+        //  actual URL it points to.
+        const resolvedPath = useResolvedPath(props.to);
+
+        // Matches a path to the location. It means actually the path passed and the location(current page address) in the browser
+        // is same or not. 
+        // end is like exact path in the Route section of the BrowserRouter.
+        const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+        return (
+            <div className={isActive ? "menuitems-active":"menuitems"} onClick={() => changes(`${props.to}`)}>
+                <span className="material-symbols-outlined">
+                    {props.symbols}
+                </span>
+                {/* <Link to="/">Ad. Sumo</Link> */}
+                {props.value}
+            </div>
+        )
+    }
+
+
+
     const changes = (route) => {
 
         //props.setStatus(false)
@@ -46,72 +72,36 @@ export default function Menus(props) {
             variants={props.variant.variantName === "MenuVaraints1" ? MenuVaraints1 : MenuVaraints2}
             initial={`${props.variant.initial}`}
             animate={`${props.variant.animate}`}
-            transition={{ type:"spring", stiffness:250}}
+            transition={{ type: "spring", stiffness: 250 }}
 
             className={props.classname}>
-            <div className="menuitems" onClick={() => changes('/triage')} >
-                <span className="material-symbols-outlined">
-                    plagiarism
-                </span>
-                {/* <Link to="/triage">Triage</Link> */}
-                Triage
-            </div>
-            <div className="menuitems" onClick={() => changes('/')}>
-                <span className="material-symbols-outlined">
-                    elderly_woman
-                </span>
-                {/* <Link to="/">Ad. Sumo</Link> */}
-                Ad. Sumo
-            </div>
-            <div className="menuitems" onClick={() => changes('/opscentral')}>
-                <span className="material-symbols-outlined">
-                    pending_actions
-                </span>
-                {/* <Link to="/opscentral">Ops Central</Link> */}
-                Ops Central
-            </div>
-            <div className="menuitems" onClick={() => changes('/criticalsystem')}>
-                <span className="material-symbols-outlined">
-                    detector_alarm
-                </span>
-                {/* <Link to="/criticalsystem">Critical System</Link> */}
-                Critical System
-            </div>
-            <div className="menuitems" onClick={() => changes('/sql')}>
-                <span className="material-symbols-outlined">
-                    key_visualizer
-                </span>
-                {/* <Link to="/sql">My Query</Link> */}
-                My Query
-            </div>
-            <div className="menuitems" onClick={() => changes('/paths')}>
-                <span className="material-symbols-outlined">
-                    travel_explore
-                </span>
-                {/* <Link to="/paths">My Paths</Link> */}
-                My paths
-            </div>
-            <div className="menuitems" onClick={() => changes('/appinsight')}>
-                <span className="material-symbols-outlined">
-                    data_thresholding
-                </span>
 
-                {/* <Link to="/appinsight">My AppInsight</Link> */}
-                My AppInsight
-            </div>
-            <div className="menuitems" onClick={() => changes('/sumo')}>
-                <span className="material-symbols-outlined">
-                    grading
-                </span>
-                {/* <Link to="/sumo">My Sumo</Link> */}
-                My Sumo
-            </div>
-            <div className="menuitems" onClick={() => changes('/sev')}>
-                <span className="material-symbols-outlined">
-                    priority_high
-                </span>
-                Sevs
-            </div>
+            <CustomLink symbols="plagiarism" value="Traige" to="/triage"/>
+            
+
+            <CustomLink symbols="elderly_woman" value="Ad. Sumo" to="/"/>
+
+
+            <CustomLink symbols="pending_actions" value="Ops Central" to="/opscentral"/>
+
+
+            <CustomLink symbols="detector_alarm" value="Critical System" to="/criticalsystem"/>
+
+
+            <CustomLink symbols="key_visualizer" value="My Query" to="/sql"/>
+
+
+            <CustomLink symbols="travel_explore" value="My paths" to="/paths"/>
+
+
+            <CustomLink symbols="data_thresholding" value="My AppInsight" to="/appinsight"/>
+
+
+            <CustomLink symbols="grading" value="My Sumo" to="/sumo"/>
+
+
+            <CustomLink symbols="priority_high" value="Sevs" to="/sev"/>
+
         </motion.div>
     )
 }
